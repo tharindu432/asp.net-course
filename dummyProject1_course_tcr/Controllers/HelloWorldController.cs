589e2a202356bf32c3348b451fd854db8428ea18
tharindu432 <tharinduchaturanga432@gmail.com>
2024-03-02 10:57:38 +0530

    initial commit - hello world controller added

---
+++ dummyProject1_course_tcr/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dummyProject1_course_tcr.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "this is the index page!";
+        }
+
+        public string hello()
+        {
+            return "who's there!?";
+        }
+
+        
+    }
+}
---
+++ dummyProject1_course_tcr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dummyProject1_course_tcr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
